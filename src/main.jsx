f72895c105917b53d94920c9ea62405005f1711b
Code With Suva <Suvamsen2004@gmail.com>
2025-03-30 21:33:32 +0530

    Redux store settup

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import { Provider } from "react-redux";
+import appStore from "./store/appStore.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <Provider store={appStore}>
       <App />
+    </Provider>
   </React.StrictMode>
 );
