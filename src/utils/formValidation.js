b0fcc0861cbf3350076271f88e900e8ee5595a1f
Code With Suva <Suvamsen2004@gmail.com>
2025-03-31 11:10:02 +0530

    fixed inconsistency in return

--- src/utils/formValidation.js
+++ src/utils/formValidation.js
@@ -1,27 +1,24 @@
+const formValidation = (username = "", email, password) => {
+  let errors = {};
 
-const formValidation = (username, email, password) => {
-    let errors = {}
-
+  if (username) {
     const validUsername = /^[a-zA-Z0-9]+$/;
-
     if (!validUsername.test(username)) {
-        errors.username = "Enter a valid username"
+      errors.username = "Enter a valid username";
     }
+  }
 
-    const validEmail =  /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  const validEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (!email || !validEmail.test(email)) {
+    errors.email = "Enter a valid email address";
+  }
 
-    if (!email || !validEmail.test(email)) {
-        errors.email = "Enter a valid email address"
-    }
+  const validPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+  if (!password || !validPassword.test(password)) {
+    errors.password = "Enter a valid password";
+  }
 
-    const validPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+  return errors;
+};
 
-    if(!password || !validPassword.test(password)) {
-        errors.password = "Enter a valid password"
-    }
-
-    return Object.keys(errors).length > 0 ? errors : null
-    
-}
-
-export default formValidation
+export default formValidation;
